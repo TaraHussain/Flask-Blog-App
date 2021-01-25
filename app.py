88bdc13159ae2174d454e3d6f6e088318d5d9bd0
Tara <TaraHussain97@gmail.com>
2021-01-25 20:55:41 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/<string:name>')
+def hello(name):
+    return "Hello" + name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
